3f2c4ae3a77127302d802c7b521e313035cf3223
Apirak Panatkool <apirak@apirak.com>
2012-04-03 14:54:36 +0700

    create second scene

--- prabu/game.js
+++ prabu/game.js
@@ -1,5 +1,5 @@
 //set main namespace
-goog.provide('brabu');
+goog.provide('game');
 
 
 //get requirements
@@ -8,68 +8,134 @@ goog.require('lime.Scene');
 goog.require('lime.Layer');
 goog.require('lime.Circle');
 goog.require('lime.Label');
-goog.require('lime.animation.Spawn');
-goog.require('lime.animation.FadeTo');
-goog.require('lime.animation.ScaleTo');
-goog.require('lime.animation.MoveTo');
+goog.require('lime.RoundedRect');
+goog.require('lime.Button');
+
+var sceneWidth = 1024;
+var sceneHeight = 768;
+var sceneCenterX = sceneWidth/2;
+var sceneCenterY = sceneHeight/2;
+
+var cardSize = 132;
+var boardSize = cardSize*3;
 
 
 // entrypoint
-brabu.start = function(){
-
-	var director = new lime.Director(document.body,1024,768),
-	    scene = new lime.Scene(),
-
-	    target = new lime.Layer().setPosition(512,384),
-        circle = new lime.Circle().setSize(150,150).setFill(255,150,0),
-        lbl = new lime.Label().setSize(160,50).setFontSize(30).setText('TOUCH ME!'),
-        title = new lime.Label().setSize(800,70).setFontSize(60).setText('Now move me around!')
-            .setOpacity(0).setPosition(512,80).setFontColor('#999').setFill(200,100,0,.1);
+game.start = function(){
 
 
-    //add circle and label to target object
-    target.appendChild(circle);
-    target.appendChild(lbl);
+  var director = new lime.Director(document.body, sceneWidth, sceneHeight);
+  var scene1 = new lime.Scene;
+  var scene2 = new lime.Scene;
 
-    //add target and title to the scene
-    scene.appendChild(target);
-    scene.appendChild(title);
+  //
+  // Scene 1
+  //
+  var background1 = new lime.Layer();
+  var background1Color = new lime.Sprite().setSize(sceneWidth, sceneHeight)
+  background1Color.setPosition(sceneCenterX, sceneCenterY).setFill(229, 0, 146);
+  background1.appendChild(background1Color);
 
-	director.makeMobileWebAppCapable();
+  var title = new lime.Sprite().setFill("assets/images/title.png");
+  title.setPosition(sceneCenterX, sceneCenterY-150);
+  background1.appendChild(title);
 
-    //add some interaction
-    goog.events.listen(target,['mousedown','touchstart'],function(e){
+  scene1.appendChild(background1);
 
-        //animate
-        target.runAction(new lime.animation.Spawn(
-            new lime.animation.FadeTo(.5).setDuration(.2),
-            new lime.animation.ScaleTo(1.5).setDuration(.8)
-        ));
+  var btn1 = new lime.Sprite().setFill('assets/images/btn_1.png');
+  var btn2 = new lime.Sprite().setFill('assets/images/btn_2.png');
 
-        title.runAction(new lime.animation.FadeTo(1));
+  var button = new lime.Button(
+    btn1,
+    btn2
+  ).setPosition(sceneCenterX,sceneCenterY+100);;
 
-        //let target follow the mouse/finger
-        e.startDrag();
+  goog.events.listen(button, 'click', function() {
+    director.replaceScene(scene2);
+  });
 
-        //listen for end event
-        e.swallow(['mouseup','touchend'],function(){
-            target.runAction(new lime.animation.Spawn(
-                new lime.animation.FadeTo(1),
-                new lime.animation.ScaleTo(1),
-                new lime.animation.MoveTo(512,384)
-            ));
-
-            title.runAction(new lime.animation.FadeTo(0));
-        });
+  scene1.appendChild(button);
 
 
-    });
+  //
+  // Scene 2
+  //
 
-	// set current scene active
-	director.replaceScene(scene);
+  var background2 = new lime.Layer();
+  var background2Color = new lime.Sprite().setSize(sceneWidth, sceneHeight)
+  background2Color.setPosition(sceneCenterX, sceneCenterY).setFill(229, 0, 146);
+  background2.appendChild(background2Color);  
+  scene2.appendChild(background2);
+
+  var board = new lime.RoundedRect().setSize(boardSize+100, boardSize+100);
+  board.setPosition(sceneCenterX-200,sceneCenterY).setFill(229,172,0);
+  board.setRadius(15,false);
+  background2.appendChild(board);
+
+  var board2 = new lime.RoundedRect().setSize(boardSize+50, boardSize+50);
+  board2.setPosition(sceneCenterX-200,sceneCenterY).setFill(51,0,0);
+  board2.setRadius(15,false);
+  background2.appendChild(board2);
+
+  var board3 = new lime.Sprite().setSize(boardSize, boardSize);
+  board3.setPosition(sceneCenterX-200,sceneCenterY).setFill(0,102,153);
+  board3.setStroke(2,255,255,255,1);
+  background2.appendChild(board3);
+
+  background2.appendChild(game.createStroke(2, boardSize, sceneCenterX-200-(boardSize/2)+cardSize, sceneCenterY));
+  background2.appendChild(game.createStroke(2, boardSize, sceneCenterX-200-(boardSize/2)+cardSize*2, sceneCenterY));  
+  background2.appendChild(game.createStroke(boardSize, 2, sceneCenterX-200, sceneCenterY-(boardSize/2)+cardSize));
+  background2.appendChild(game.createStroke(boardSize, 2, sceneCenterX-200, sceneCenterY-(boardSize/2)+cardSize*2)); 
+
+  var title = new lime.Sprite().setScale(0.7,0.7);
+  title.setFill("assets/images/title.png");
+  title.setPosition(200, 100);
+  background2.appendChild(title);
+
+  game.addCardNumber(background2);  
+  game.addCardPicture(background2);
+  // set current scene active
+  director.makeMobileWebAppCapable();
+  director.replaceScene(scene1);
+
 
 }
 
+game.addCardNumber = function(layer) {
+  cardNumber = 1;
+  var topX = sceneCenterX - 200 - (boardSize/2) + cardSize/2 - 5;
+  var topY = sceneCenterY - (boardSize/2) + cardSize/2 - 10;
+  for(i=0; i<3; i++) {
+    for(j=0; j<3; j++) {
+      var number = new lime.Label().setSize(5,5).setFontSize(30);
+      number.setFontColor('#FFFFFF');
+      number.setText(cardNumber);
+      number.setPosition(topX + cardSize*j, topY + cardSize*i);
+      layer.appendChild(number);
+      cardNumber++;
+    }
+  }
+}
+
+game.addCardPicture = function(layer) {
+  cardNumber = 1;
+  x = [100, 200, 150, 250, 123, 256];
+  y = [120, 300, 500, 400, 203, 450];
+
+  for (i=0; i<6; i++) {
+    var card = new lime.Sprite().setFill('assets/images/image_'+cardNumber+'.png');
+    card.setPosition(x[i]+550, y[i]);
+    layer.appendChild(card);
+    cardNumber++;
+  }
+}
+
+game.createStroke = function(w, h, x, y){
+  var stroke = new lime.Sprite().setSize(w, h);
+  stroke.setPosition(x, y);
+  stroke.setFill(255,255,255);
+  return stroke;
+}
 
 //this is required for outside access after code is compiled in ADVANCED_COMPILATIONS mode
-goog.exportSymbol('brabu.start', brabu.start);
+goog.exportSymbol('game.start', game.start);
